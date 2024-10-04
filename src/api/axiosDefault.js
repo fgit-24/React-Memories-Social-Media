acfa37d1cf2ee8ffd6208b090d49a6d81191715c
Florian Hass <f91hama@gmail.com>
2024-10-04 10:37:30 +0000

    Implement useContext hook to communicate with child components

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://drf-api-moments-f-72ce153a1bda.herokuapp.com/'
-axios.defaults.headers.post['Çontent-Type'] = 'multipart/form-data'
+axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
