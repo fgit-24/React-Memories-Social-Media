727ace6c2665fe6620eb1f9aa324f51634fd4a6f
Florian Hass <f91hama@gmail.com>
2024-10-05 19:22:23 +0000

    feat: Add infinite scroll to PostPage and implement follow/unfollow functionality
    - Added `handleUnfollow` function to manage unfollowing profiles.
    - Implemented infinite scrolling for comments on the PostPage using `InfiniteScroll` and `fetchMoreData`.
    - Modified the `PostPage` to dynamically load more comments as the user scrolls.
    - Updated helper functions `followHelper` and `unfollowHelper` to manage profile follow/unfollow state.
    - Enhanced UI to display a spinner while loading more comments.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
+
 import { axiosReq } from "../api/axiosDefaults";
 
 export const fetchMoreData = async (resource, setResource) => {
@@ -13,4 +14,34 @@ export const fetchMoreData = async (resource, setResource) => {
       }, prevResource.results),
     }));
   } catch (err) {}
+};
+
+export const followHelper = (profile, clickedProfile, following_id) => {
+  return profile.id === clickedProfile.id
+    ? 
+      {
+        ...profile,
+        followers_count: profile.followers_count + 1,
+        following_id,
+      }
+    : profile.is_owner
+    ? 
+      { ...profile, following_count: profile.following_count + 1 }
+    : 
+      profile;
+};
+
+export const unfollowHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ? 
+      {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ? 
+      { ...profile, following_count: profile.following_count - 1 }
+    : 
+      profile;
 };
