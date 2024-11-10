7e4f7972bf5ab74756d1941d3cbd9ca5028b83df
Florian Hass <f91hama@gmail.com>
2024-11-10 08:05:42 +0000

    Refactor axios

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Set axios defaults
-axios.defaults.baseURL = "https://social-api-f-f73ebdfcae43.herokuapp.com/";
+axios.defaults.baseURL = "https://8000-fgit24-pp5djangorestfra-8jjaed52v4m.ws.codeinstitute-ide.net/";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true;
 
